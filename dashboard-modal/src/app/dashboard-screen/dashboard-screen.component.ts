73fc649b6413affd11c0130b84ced53837b905b8
Bharghav B <bharghav.b@asg.com>
2020-08-20 16:31:08 +0530

    Removed chartjs and used ng2-charts approach for dynamic data rendering

--- dashboard-modal/src/app/dashboard-screen/dashboard-screen.component.ts
+++ dashboard-modal/src/app/dashboard-screen/dashboard-screen.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { AppserviceService } from '../appservice.service';
 import { FormControl } from '@angular/forms';
+import { ChartDataSets, ChartOptions } from 'chart.js';
+import { Color, Label } from 'ng2-charts';
+
 
 @Component({
   selector: 'app-dashboard-screen',
@@ -10,8 +13,55 @@ import { FormControl } from '@angular/forms';
 export class DashboardScreenComponent implements OnInit {
   data: any;
   name = new FormControl('select');
-  chartsTypes = ['Bar', 'Graph', 'Pie'];
+  chartsTypes = ['bar', 'radar', 'pie', 'line'];
   chartsType: any;
+  Label = [];
+  Data = [];
+  isToggle: boolean = false;
+  columnDefs = [
+    { field: 'Time' },
+    { field: 'Temperature' }
+  ];
+  chartData: ChartDataSets[] = [
+    {
+      data: this.Data,
+      lineTension: 0,
+      borderWidth: 2,
+      pointRadius: 2,
+      pointBackgroundColor: 'white',
+      cubicInterpolationMode: 'monotone'
+    }
+  ];
+
+  chartOptions: ChartOptions = {
+    responsive: true,
+    legend: {
+      display: true,
+      position: "right",
+      labels: {
+        fontColor: "#000080",
+      },
+      align: "start"
+    },
+    scales: {
+      yAxes: [{
+        ticks: {
+          min: 0,
+          max: 100,
+        }
+      },
+      ]
+    }
+  };
+  chartLabels: Label[] = this.Label;
+  chartColors: Color[] = [
+    {
+      borderColor: 'rgb(255, 153, 255)',
+      backgroundColor: 'rgba(255, 153, 255,0.1)',
+    }
+  ];
+  chartLegend = true;
+  chartType = 'line';
   constructor(private appserviceService: AppserviceService) { }
 
   ngOnInit() {
@@ -19,10 +69,20 @@ export class DashboardScreenComponent implements OnInit {
   }
   getChartData(): void {
     this.data = this.appserviceService.getData();
+    this.data.forEach(i => {
+      this.Data.push(i.Temperature);
+      this.Label.push(i.Time);
+    });
   }
-
+  onValChange() {
+    this.isToggle = !this.isToggle;
+  }
   changeChart(e) {
     this.chartsType = e.target.value;
-    alert(e.target.value);
+    this.chartType = this.chartsType;
+    this.chartLabels = this.Label;
+    this.chartData.forEach(item => {
+      item.data = this.Data
+    });
   }
 }
